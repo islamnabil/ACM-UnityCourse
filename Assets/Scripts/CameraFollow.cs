3ee3ce231e0865168908df0d01eb9a3a47aa8dca
Islam Nabil <islamelgaafary@gmail.com>
2019-07-21 18:21:59 +0200

    Day#2

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject player;
+
+    public float cameraHeight = 20.0f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = player.transform.position;
+
+        pos.z -=10;
+        pos.y += 5;
+
+        transform.position = pos;
+        
+    }
+}
---
+++ Assets/Scripts/ShpereCollision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShpereCollision : MonoBehaviour
+{
+
+     Material sphereMaterial;
+     AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sphereMaterial = GetComponent<Renderer>().material;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            sphereMaterial.color = Color.red;
+            audioSource.Play();
+        }
+       
+    }
+    
+}
